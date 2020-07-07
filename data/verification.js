2cd7383272e9f4dd45ff27bbeebc67c7fbe133ee
Kenny Vu <kennyvu@live.ca>
2020-07-07 14:48:40 -0400

    First 3 requirements done!

---
+++ data/verification.js
@@ -0,0 +1,37 @@
+const { customers, stock } = require("./promo");
+
+class verification {
+  constructor() {
+    this.country = "Canada";
+  }
+  checkCountry = (country) => {
+    return country === this.country ? true : false;
+  };
+  checkName = (givenName, surName) => {
+    return customers.some((customer) => {
+      return givenName === customer.givenName && surName === customer.surname;
+    });
+  };
+  checkEmail = (email) => {
+    return customers.some((customer) => {
+      return email === customer.email;
+    });
+  };
+  checkAddress = (address, country) => {
+    return customers.some((customer) => {
+      return address === customer.address && country !== this.country;
+    });
+  };
+  checkStock = (order, size) => {
+    if (order === "shirt") {
+      const shirtSize = size;
+      return stock.shirt[`${shirtSize}`] > 0;
+    } else {
+      return stock[`${order}`] > 0;
+    }
+  };
+}
+
+const doubleCheck = new verification();
+
+module.exports = { doubleCheck };
--- server.js
+++ server.js
@@ -1,25 +1,45 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const { doubleCheck } = require("./data/verification");
+
+const handleForm = (req, res) => {
+  const { address, email, surname, givenName, country, order, size } = req.body;
+  const validCountry = doubleCheck.checkCountry(country);
+  const invalidAddress = doubleCheck.checkAddress(address);
+  const invalidName = doubleCheck.checkName(givenName, surname);
+  const invalidEmail = doubleCheck.checkEmail(email);
+  const isInStock = doubleCheck.checkStock(order, size);
+  if (!validCountry) {
+    res.json({ status: "error", error: "undeliverable" });
+  } else if (invalidAddress || invalidEmail || invalidName) {
+    res.json({ status: "error", error: "repeat-customer" });
+  } else if (isInStock === false) {
+    res.json({ status: "error", error: "unavailable" });
+  } else {
+    res.json({ status: "success" });
+  }
+};
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
-
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .post("/order", handleForm)
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(8000, () => console.log(`Listening on port 8000`));
